2a52cd65453e52dd8380887c7cdce0a541a36330
Ayush Bhatt <ayushbhatt197@gmail.com>
2020-07-20 12:15:24 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./utils/App";
 import "font-awesome/css/font-awesome.css";
